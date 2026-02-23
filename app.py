d0f54a3f7d0110385e361ccbd188d21403918708
aryauprale <upralearya@gmail.com>
2026-02-23 14:25:11 -0600

    Configured files and blended some html files together

--- app.py
+++ app.py
@@ -2,41 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+@app.route("/index")
 def home():
-    return render_template("index.html", page_css="style1.css")
+    return render_template("index.html")
 
 @app.route("/aboutus")
 def about():
-    return render_template("aboutus.html", page_css="style1.css")
+    return render_template("aboutus.html")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
