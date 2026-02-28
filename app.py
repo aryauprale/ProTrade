bf9be511e8e62a9b0faec6e3322906f5591dc756
aryauprale <upralearya@gmail.com>
2026-02-27 23:45:53 -0600

    working on app

--- app.py
+++ app.py
@@ -12,5 +12,24 @@ def about():
     return render_template("aboutus.html")
 
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/reset", methods=["GET", "POST"])
+def reset():
+    if request.method == "POST":
+        # later I will validate phone/otp and redirect
+        return redirect(url_for("verify"))
+    return render_template("reset.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", debug_marker="TEMPLATE LOADED")
+
 if __name__ == "__main__":
     app.run(debug=True)
